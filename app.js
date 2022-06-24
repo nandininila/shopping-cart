e4cc4622c1a717174f8f6a680475eac028ffbfdd
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-06-24 22:53:14 +0600

    change all

---
+++ app.js
@@ -0,0 +1,35 @@
+function updateCaseNumber(product, price, isIncreasing) {
+
+
+  const caseInput = document.getElementById(product + "-number");
+
+  let caseNumber = caseInput.value;
+  
+  if (isIncreasing == true) {
+    caseNumber = parseInt(caseNumber) + 1;
+  } else if (caseNumber > 0) {
+    caseNumber = parseInt(caseNumber) - 1;
+  }
+
+  caseInput.value = caseNumber;
+
+  // update case total
+  const caseTotal = document.getElementById(product + "-total");
+  caseTotal.innerText = caseNumber * price;
+}
+
+document.getElementById("case-plus").addEventListener("click", function () {
+  updateCaseNumber('case', 59, true);
+});
+
+document.getElementById("case-minus").addEventListener("click", function () {
+  updateCaseNumber('case', 59, false);
+});
+
+document.getElementById("phone-plus").addEventListener("click", function () {
+  updateCaseNumber('phone', 1219, true);
+});
+
+document.getElementById("phone-minus").addEventListener("click", function () {
+  updateCaseNumber('phone', 1219, false);
+});
